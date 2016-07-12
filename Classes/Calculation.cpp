e4bf5ad7d539e5da99b11537bc9bf8dad784cd7f
SikSakJiiPee <juissi93@hotmail.com>
2016-07-13 01:40:24 +0300

    Better RNG and calculations for correct answers

--- Classes/Calculation.cpp
+++ Classes/Calculation.cpp
@@ -1,8 +1,4 @@
 #include "Calculation.h"
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <cstdlib>
 
 cocos2d::Scene* Calculation::createScene()
 {
@@ -17,24 +13,20 @@ bool Calculation::init()
 	if (!Layer::init())
 		return false;
 
-	//int number = randomNumber();
-	//std::stringstream ss;
-	//ss << number;
-	//std::string str = ss.str();
+	int number = randomNumber();
+	std::string sNumber = convertIntToString(number);
 
 	int number2 = randomNumber();
-	std::stringstream ss2;
-	ss2 << number2;
-	std::string str2 = ss2.str();
+	std::string sNumber2 = convertIntToString(number2);
 
-	std::string str = "1";
+	int correctAnswer = getCorrectAnswerPlus(number, number2);
+	std::string sCorrectAnswer = convertIntToString(correctAnswer);
 
 	auto label = cocos2d::Label::createWithSystemFont("Calculation", "Arial", 30);
 	label->setPosition(cocos2d::Vec2(this->getBoundingBox().getMidX(), this->getBoundingBox().getMaxY() - 20));
 	this->addChild(label, 1);
 
-	//auto label2 = cocos2d::Label::createWithSystemFont("1+1=?", "Arial", 30);
-	auto label2 = cocos2d::Label::createWithSystemFont(str + " + " + str2, "Arial", 30);
+	auto label2 = cocos2d::Label::createWithSystemFont(sNumber + " + " + sNumber2 + " = " + sCorrectAnswer, "Arial", 30);
 	label2->setPosition(cocos2d::Vec2(this->getBoundingBox().getMidX(), this->getBoundingBox().getMidY()));
 	this->addChild(label2, 2);
 
@@ -59,7 +51,33 @@ bool Calculation::init()
 
 int Calculation::randomNumber()
 {
-	int rNumber = rand() % 100;
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> uni(1, 100);
+	auto rNumber = uni(rng);
 
 	return rNumber;
+}
+
+int Calculation::getCorrectAnswerPlus(int number, int number2)
+{
+	int correctAnswer = number + number2;
+
+	return correctAnswer;
+}
+
+int Calculation::getCorrectAnswerMinus(int number, int number2)
+{
+	int correctAnswer = number - number2;
+
+	return correctAnswer;
+}
+
+std::string Calculation::convertIntToString(int intNumber)
+{
+	std::stringstream ss;
+	ss << intNumber;
+	std::string stringNumber = ss.str();
+
+	return stringNumber;
 }
--- Classes/Calculation.h
+++ Classes/Calculation.h
@@ -2,11 +2,25 @@
 
 #include "cocos2d.h"
 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdio> //ei v‰ltt‰m‰tt‰ tarvita en‰‰ (rand)
+#include <cstdlib> //ei v‰ltt‰m‰tt‰ tarvita en‰‰ (srand)
+#include <time.h>
+#include <random>
+
+
 class Calculation : public cocos2d::Layer
 {
 public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
+
 	int randomNumber();
+	int getCorrectAnswerPlus(int number, int number2);
+	int getCorrectAnswerMinus(int number, int number2);
+	std::string convertIntToString(int number);
+
 	CREATE_FUNC(Calculation);
 };
